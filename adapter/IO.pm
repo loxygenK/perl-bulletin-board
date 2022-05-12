bd625781d8c4bf003b2abe408c41af1ddb020343
loxygenk <me@loxygen.dev>
2022-05-12 12:14:02 +0900

    fix: prepare directories before RW

--- adapter/IO.pm
+++ adapter/IO.pm
@@ -7,7 +7,7 @@ our @EXPORT_OK = qw(read_all write_file store);
 our @EXPORT = qw(read_all write_file store);
 
 sub read_all {
-  open FILE, $_[0] or die "Could not open file '$_[0]'";
+  open FILE, prepare_directory($_[0]) or die "Could not open file '$_[0]'";
   my $buf = "";
 
   while(<FILE>) {
@@ -26,5 +26,20 @@ sub write_file {
 }
 
 sub store {
-  return "../storage/$_[0]"
+  return prepare_directory("../storage/$_[0]")
+}
+
+sub prepare_directory {
+  my @dir_elem = split(/\//, $_[0]);
+
+  my $current_path = "";
+  for(my $i = 0; $i < (scalar @dir_elem - 1); $i++) {
+    $current_path = $current_path . $dir_elem[$i] . "/";
+
+    if(! -e $current_path) {
+      mkdir($current_path) or die "FATAL: Could not prepare directory at '$current_path'"
+    }
+  }
+
+  return $_[0];
 }
