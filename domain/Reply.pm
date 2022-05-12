c664f8296e964936063e5c0827f162be3cd8b69a
loxygenk <me@loxygen.dev>
2022-05-12 09:55:03 +0900

    feat: create Reply domain obj

---
+++ domain/Reply.pm
@@ -0,0 +1,27 @@
+package Reply;
+
+use strict;
+use warnings;
+
+use Exporter;
+our @ISA = qw(Exporter);
+
+sub new {
+  return (
+    post_id => $_[0],
+    author => $_[1],
+    content => $_[2]
+  );
+}
+
+sub post_id {
+  return %{$_[0]}{"post_id"}
+}
+
+sub author {
+  return %{$_[0]}{"post_id"}
+}
+
+sub content {
+  return %{$_[0]}{"post_id"}
+}
